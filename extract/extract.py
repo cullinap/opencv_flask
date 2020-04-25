82e1497a01739c8a1652c302ec1c56c84337078e
cullinap <cullinap@gmail.com>
2020-04-24 22:51:00 -0400

    13 - add haarcascades

--- extract/extract.py
+++ extract/extract.py
@@ -6,8 +6,12 @@ img_path = os.path.join(BASE_DIR, 'espn.png')
 
 frame = cv2.imread(img_path)
 gray  = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+cascade_path = os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml')
+cascade = cv2.CascadeClassifier(cascade_path)
+faces = cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+print(faces)
 cv2.imshow("frame", frame)
 cv2.imshow("gray", gray)
 cv2.waitKey(0) # press w to quit
-cv2.destroyAllWindowns()
+cv2.destroyAllWindows()
+
